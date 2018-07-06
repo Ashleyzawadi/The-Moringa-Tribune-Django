7058156643c3c66de82bba7f325494daa0855e1d
ashleyzawadi <zawadikibathi@gmail.com>
2018-07-06 17:36:02 +0300

    Initial project setup, views, models

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ashley/Documents/School/LMS/The-Moringa-Tribune/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
